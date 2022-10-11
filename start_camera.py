027663351b6571d63229b0a4da15beee6a1bd8d4
Reza Vilera <vilera_reza99@yahoo.com>
2022-10-11 08:35:00 +0700

    changing start_camera.py

--- start_camera.py
+++ start_camera.py
@@ -64,13 +64,15 @@ async def on_connect(websocket):
     print ('Client connected')
 
     async def receive(websocket):
-        while True:
-            print ('receiving')
-            try:
-                message = await websocket.recv()
-            except websockets.ConnectionClosedOK:
-                break
+        async for message in websocket:
             print (message)
+        # while True:
+        #     print ('receiving')
+        #     try:
+        #         message = await websocket.recv()
+        #     except websockets.ConnectionClosedOK:
+        #         break
+        #     print (message)
     
     def wait (output):
         with output.condition:
@@ -82,15 +84,12 @@ async def on_connect(websocket):
             try:
                 frame = await asyncio.to_thread(wait, output)
                 await websocket.send(frame)
-                
-                async for message in websocket:
-                    print (message)
 
             except websockets.ConnectionClosedOK:
                 break
 
     await send(websocket)
-    #await receive(websocket)
+    await receive(websocket)
 
 
 async def ws_to_client():
