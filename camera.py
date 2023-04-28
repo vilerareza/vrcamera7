85973546c5308a73dcbad5f88cb2041de5ba4cb5
Reza Vilera <vilera_reza99@yahoo.com>
2023-04-28 23:03:03 +0700

    Adding internal recording

--- camera.py
+++ camera.py
@@ -13,6 +13,7 @@ class Camera():
             self.camera.contrast = 0
             self.camera.sharpness = 50
             self.camera.start_recording(output, format='mjpeg')
+            self.camera.start_recording('test_rec.h264', splitter_port=2, resize = (1280, 720), quality=25)
             print('Camera is started')
         else:
             print('Camera is already started') 
@@ -21,6 +22,7 @@ class Camera():
         if self.camera:
             try:
                 self.camera.stop_recording()
+                self.camera.stop_recording(splitter_port=2)
                 self.camera.close()
                 self.camera = None
                 status = b'stop_ok'
