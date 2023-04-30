fd1a07f16fab69ad3e701a4e5cd7c75f83309e75
Reza Vilera <vilera_reza99@yahoo.com>
2023-04-30 22:59:00 +0700

    internal recording

--- get_rec_file.py
+++ get_rec_file.py
@@ -2,10 +2,13 @@ import os
 import subprocess
 
 
-def get_rec_file(file_name):
+def get_rec_file(file_path, fps=20):
 
-    out_file = f'{os.path.splitext(os.path.split(file_name)[-1])[0]}.mp4'
+    dir_name = os.path.split(file_path)[0]
+    file_name = os.path.splitext(os.path.split(file_name)[-1])[0]
+
+    out_file = f'{os.path.join(dir_name, file_name)}.mp4'
 
     result = subprocess.run(['MP4Box',
                     '-add', file_name,
-                    ':fps=20', out_file])
+                    f':fps={fps}', out_file])
