6db70f5d1192a59ed01a8b034a55ac3407c0dfa0
Reza Vilera <vilera_reza99@yahoo.com>
2023-04-30 23:02:33 +0700

    internal recording

--- get_rec_file.py
+++ get_rec_file.py
@@ -5,7 +5,7 @@ import subprocess
 def get_rec_file(file_path, fps=20):
 
     dir_name = os.path.split(file_path)[0]
-    file_name = os.path.splitext(os.path.split(file_name)[-1])[0]
+    file_name = os.path.splitext(os.path.split(file_path)[-1])[0]
 
     out_file = f'{os.path.join(dir_name, file_name)}.mp4'
 
