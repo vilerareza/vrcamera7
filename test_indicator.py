8aa1432337aac19893b2164e3a6a7d3b3e426b44
Reza Vilera <vilera_reza99@yahoo.com>
2023-07-04 20:01:42 +0700

    adding led indicator test script

--- test_indicator.py
+++ test_indicator.py
@@ -2,7 +2,8 @@ import asyncio
 
 from indicator import Indicator
 
-blue_indicator = Indicator(pin=17)
+blue_indicator = Indicator(pin=27)
+yellow_indicator = Indicator(pin=22)
 indicator_off = False
 
 async def blink_indicator(indicator):
@@ -12,6 +13,11 @@ async def blink_indicator(indicator):
         await asyncio.sleep(3)
         indicator.off()
         await asyncio.sleep(3)
+
+async def error_indicator(indicator):
+    await asyncio.sleep(5)
+    indicator.on()
+
 
 async def stop_indicator(indicator, timeout):
     global indicator_off
@@ -23,6 +29,7 @@ async def stop_indicator(indicator, timeout):
 async def main():
     # Blinking indicator
     task_blink_indicator = asyncio.create_task(blink_indicator(blue_indicator))
+    task_error_indicator = asyncio.create_task(error_indicator(yellow_indicator))
     task_stop_indicator = asyncio.create_task(stop_indicator(blue_indicator, 20))
 
     await task_blink_indicator
