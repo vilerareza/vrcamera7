a61e34dc78e1eff11410388d07252cd971d9715c
Reza Vilera <vilera_reza99@yahoo.com>
2023-08-28 09:48:04 +0700

    Switch to picamera2

--- camera.py
+++ camera.py
@@ -74,7 +74,7 @@ class Camera():
             # Applying configuration
             self.camera.configure(config)
             # Setting the controls
-            #self.camera.set_controls({'Sharpness': 8})
+            self.camera.set_controls({'Sharpness': 8})
             # Starting the camera
             encoder = picamera2.encoders.JpegEncoder()
             # self.camera.start()
@@ -83,7 +83,7 @@ class Camera():
             output_ = FileOutput(output)
             #self.on_indicator.on()
             #self.error_indicator.off()
-            self.camera.start_recording(encoder, output_)
+            self.camera.start_recording(encoder, [output_, 'test3.h264'])
             print('Camera is started')
 
 
