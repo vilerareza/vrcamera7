34485a84f833b0fcbf5f097172bf1fea56580978
Reza Vilera <vilera_reza99@yahoo.com>
2023-08-30 09:31:23 +0700

    switch to picamera2

--- camera.py
+++ camera.py
@@ -76,8 +76,8 @@ class Camera():
             # Setting the controls
             self.camera.set_controls({'Sharpness': 8})
             self.camera.set_controls({'ScalerCrop': [10,10, 500, 500]})
-            self.camera.options['quality'] = 1
-            self.camera.options['compress_level'] = 1
+            self.camera.options['quality'] = 95
+            self.camera.options['compress_level'] = 9
             # Starting the camera
             encoder = picamera2.encoders.JpegEncoder()
             self.recording = True
