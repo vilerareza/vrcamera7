e96d887fd402e115964a688c383deeb05bfe6af4
Reza Vilera <vilera_reza99@yahoo.com>
2023-09-01 09:36:38 +0700

    switch to picamera2

--- camera.py
+++ camera.py
@@ -75,7 +75,7 @@ class Camera():
             print (f'full res: {full_res}')
             # Setting the controls
             self.camera.set_controls({'Sharpness': 8})
-            self.camera.set_controls({'ScalerCrop': [0,0,1920,1080]})
+            self.camera.set_controls({'ScalerCrop': [10,10,1920,1080]})
             self.camera.options['quality'] = 95
             self.camera.options['compress_level'] = 9
             # Starting the camera
